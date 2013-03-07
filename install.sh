9f317f9beeee85e6a6232a8988a72b1048bcbdca
Maxim Dementyev <max@orofarne.ru>
2013-03-07 15:34:42 +0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PREFIX="/opt/hammy"
+
+set -ex
+
+SRCROOT=`dirname "$(pwd)/$0"`
+
+mkdir -p "$PREFIX"
+cp -R "$SRCROOT/src" "$SRCROOT/ruby" "$PREFIX"
+
+export GOPATH="$PREFIX"
+cd "$PREFIX"
+go run "$SRCROOT/bootstrap.go"
+cd -
