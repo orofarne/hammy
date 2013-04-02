dd948e98a57efc36b0eb5688daa17c58aeaf696f
Maxim Dementyev <max@orofarne.ru>
2013-04-03 00:56:59 +0400

    build scripts fixed

--- build.sh
+++ build.sh
@@ -14,3 +14,12 @@ cd -
 . ./env_ex.sh
 go run bootstrap.go
 go test hammy && go install hammy hammyd hammycid hammydatad
+
+# Setup worker
+cd worker
+mkdir build
+cd build
+cmake ..
+make
+make test
+cd ../..
