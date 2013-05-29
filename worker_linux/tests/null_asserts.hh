5281d2eaef069b66d65a968456adeed49f843fef
Maxim Dementyev <max@orofarne.ru>
2013-05-29 04:14:26 +0400

    old code removed

--- worker_linux/tests/null_asserts.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ASSERT_NULL(p) ASSERT_EQ (NULL, (long)p)
-#define ASSERT_NOT_NULL(p) ASSERT_NE (NULL, (long)p)
-
-#define EXPECT_NULL(p) EXPECT_EQ (NULL, (long)p)
-#define EXPECT_NOT_NULL(p) EXPECT_NE (NULL, (long)p)
